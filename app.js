fd8de54f66810e2f4b60305a5f51c0ab737ca79a
Hamza Dahmani <madebyhamzadahmani@gmail.com>
2021-09-29 14:37:39 +0100

    speed

--- app.js
+++ app.js
@@ -27,45 +27,41 @@
 // //     geolocationCoordinatesInstance.speed;
 // // }, 1000);
 const x = document.querySelector('body');
-// // setInterval(() => {
+setInterval(() => {
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      x.innerHTML = 'Geolocation is not supported by this browser.';
+    }
+  }
 
-// function getLocation() {
-//   if (navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition({
-//       timeout: 30000,
-//       enableHighAccuracy: false,
-//     });
-//   } else {
-//     x.innerHTML = 'Geolocation is not supported by this browser.';
-//   }
-// }
-
-// function showPosition(position) {
-//   setInterval(() => {
-//     x.innerHTML += 'Speed: ' + position.coords.speed + '<br>';
-//   }, 500);
-// }
-// window.addEventListener('load', () => getLocation());
-// // }, 500);
+  function showPosition(position) {
+    setInterval(() => {
+      x.innerHTML += 'Speed: ' + position.coords.speed + '<br>';
+    }, 500);
+  }
+  window.addEventListener('load', () => getLocation());
+}, 500);
 
 ////////////////////
 
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(success, fail);
-} else {
-  alert('Sorry, your browser does not support geolocation services.');
-}
+// if (navigator.geolocation) {
+//   navigator.geolocation.getCurrentPosition(success, fail);
+// } else {
+//   alert('Sorry, your browser does not support geolocation services.');
+// }
 
-function success(position) {
-  x.innerHTML =
-    position.coords.latitude +
-    'lon=' +
-    position.coords.longitude +
-    'accuracy' +
-    position.coords.accuracy;
-}
+// function success(position) {
+//   x.innerHTML =
+//     position.coords.latitude +
+//     'lon=' +
+//     position.coords.longitude +
+//     'accuracy' +
+//     position.coords.accuracy;
+// }
 
-function fail() {
-  //
-  x.innerHTML = 'Could not obtain location';
-}
+// function fail() {
+//   //
+//   x.innerHTML = 'Could not obtain location';
+// }
